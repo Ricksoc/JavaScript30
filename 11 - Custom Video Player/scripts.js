13948e08f2b72def3886d3d14df4a238e77ea4a4
ricky <ricky.chaggar@gmail.com>
2021-03-25 17:04:44 +0000

    completed challenge 11 as per video. Added controls for volume and playback rate as well as setting video progress with slide bar

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,7 +4,7 @@
 const player = document.querySelector(".player");
 const video = player.querySelector(".viewer");
 const progress = player.querySelector(".progress");
-const progressBar = player.querySelector(".progress-filled");
+const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
@@ -27,6 +27,20 @@ function timeSkip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTIme = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTIme;
+}
+
 /* Connect event listners */
 // eventListeners for clicking on button or video screen
 toggle.addEventListener("click", togglePlay);
@@ -34,24 +48,33 @@ video.addEventListener("click", togglePlay);
 // Event listeners to change button icon when video is played / paused
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
+// Add event listener to update time
+video.addEventListener("timeupdate", handleProgress);
+
+// Set up ability to move video time using progress bar
+let mousedown = false;
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
+progress.addEventListener("click", scrub);
+
+// function first checks mousedown and if true proceeds onto scrub
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+
 // Event listeners to allow spacebar to play / pause video after button has been clicked
 document.addEventListener("keyup", (e) => {
   if (e.code === "Space") {
     togglePlay();
-    console.log("document");
   }
 });
 toggle.addEventListener("keyup", (e) => {
   if (e.code === "Space") {
     togglePlay();
-    console.log("toggle");
   }
 });
 
 skipButtons.forEach((skip) => skip.addEventListener("click", timeSkip));
 
+// Add event listener to detect when volue / playback rate are changed
 ranges.forEach((slider) =>
-  slider.addEventListener("click", (e) => {
-    console.log(e.target);
-  })
+  slider.addEventListener("change", handleRangeUpdate)
 );
