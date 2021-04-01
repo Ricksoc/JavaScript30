8c79093b93f21129ce56fef8e76350a2bb74eff8
ricky <ricky.chaggar@gmail.com>
2021-04-01 14:57:40 +0100

    Completed base challenge learning localStorage and event delegation. Not yet tried adding clear, check and uncheck buttons

---
+++ 15 - LocalStorage/index.js
@@ -0,0 +1,52 @@
+// jshint esversion: 8
+
+const addItems = document.querySelector(".add-items");
+const itemsList = document.querySelector(".plates");
+const items = JSON.parse(localStorage.getItem("items")) || [];
+
+function addItem(e) {
+  // by default submitting a form refreshes the page
+  e.preventDefault();
+  const text = this.querySelector("[name=item]").value;
+  const item = {
+    text, // es:6 shorthand allows this to be used instead of text: text
+    done: false,
+  };
+  items.push(item);
+  populateList(items, itemsList);
+  localStorage.setItem("items", JSON.stringify(items));
+  this.reset(); // resest form
+}
+
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates
+    .map((plate, i) => {
+      return `
+    <li>
+    <input type="checkbox" data-index=${i} id="item${i}" ${
+        plate.done ? "checked" : ""
+      }/>
+    <label for="item${i}">${plate.text}</label>
+    </li>
+    `;
+    })
+    .join("");
+}
+
+function toggleDone(e) {
+  if (!e.target.matches("input")) return; // skip this unless it's an input
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+addItems.addEventListener("submit", addItem);
+// add event listener to the <ul> which is always present nomatter how many list items
+// this is called event delegation
+itemsList.addEventListener("click", toggleDone);
+
+setTimeout(() => {
+  populateList(items, itemsList);
+}, 200);
